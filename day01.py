40f16b65664abe1fef877065196c5fee4cd86557
Charlie Garavaglia <charlieg895@gmail.com>
2022-12-14 08:42:49 -0600

    day 1 refactored counter solution
    also pytest fixture for sample input test best practice

--- day01.py
+++ day01.py
@@ -1,25 +1,20 @@
+from utils import INPUT_DIR
 from pathlib import Path
 from collections import Counter
+import pytest
 
-data_path = Path("input/data01")
-
-
-def part1(p: Path = data_path):
-    most_cals = 0
-    with p.open() as f:
-        elf_cals = 0
-        for line in f:
-            if line != "\n":
-                elf_cals += int(line)
-            else:
-                if elf_cals > most_cals:
-                    most_cals = elf_cals
-                elf_cals = 0
-                continue
-    return most_cals
+data_path = INPUT_DIR / "data01"
 
 
-def part2(p: Path = data_path):
+def part1(elf_counter: Counter):
+    return max(elf_counter.values())
+
+
+def part2(elf_counter: Counter):
+    return sum(x[1] for x in elf_counter.most_common(3))
+
+
+def common_counter(p: Path = data_path):
     elf_i = 0
     elves = Counter()
     with p.open() as f:
@@ -29,19 +24,24 @@ def part2(p: Path = data_path):
             else:
                 elf_i += 1
                 continue
-    return sum(x[1] for x in elves.most_common(3))
+    return elves
 
 
 if __name__ == "__main__":
-    print(part1())
-    print(part2())
-
-sample_data_path = Path("input/sample01")
-
-
-def test_sample_input_part1():
-    assert part1(sample_data_path) == 24000
+    elf_counter = common_counter()
+    print(part1(elf_counter))
+    print(part2(elf_counter))
 
 
-def test_sample_input_part2():
-    assert part2(sample_data_path) == 45000
+@pytest.fixture
+def sample_elf_counter():
+    sample_data_path = INPUT_DIR / ("sample01")
+    return common_counter(sample_data_path)
+
+
+def test_sample_input_part1(sample_elf_counter):
+    assert part1(sample_elf_counter) == 24000
+
+
+def test_sample_input_part2(sample_elf_counter):
+    assert part2(sample_elf_counter) == 45000
